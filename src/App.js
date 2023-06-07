12cdae9908ac5c13345d93ebd425bb0a5337acee
francomacedo96 <francomacedo@outlook.com>
2023-06-07 16:04:18 -0300

    suba

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import reactLogo from "./imagenes/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contador-logo-contenedor'>
+        <img className='contador-logo' 
+        src={reactLogo} alt="" />
+      </div>
+    </div> 
+  );
+}
+
+export default App;
